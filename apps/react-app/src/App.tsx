f808f8877586c8a8cff18c1034ac20e50187a390
장명수 <msjang24@jasonsystem.co.kr>
2025-05-19 14:10:44 +0900

    fix: lint test

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -11,7 +11,6 @@ function App() {
           <Button variant="juiGrey">jui Grey</Button>
           <Button variant="juiGradient">jui Button Gradient</Button>
           <LogInIcon />
-          <CheckIcon />
         </div>
       </div>
     </main>
