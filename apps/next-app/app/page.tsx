010bd38b9b8126d3e2cf1c9eaab40ea1c88455b9
장명수 <msjang24@jasonsystem.co.kr>
2025-05-19 14:12:50 +0900

    fix: lint test

--- apps/next-app/app/page.tsx
+++ apps/next-app/app/page.tsx
@@ -178,7 +178,6 @@ export default function Page() {
           <PlusCircleIcon />
           <PlusIcon />
           <SideBarIcon />
-          <TrashIcon />
         </div>
       </div>
     </div>
