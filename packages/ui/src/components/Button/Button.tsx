d7a3553d634c3f61d7dc82f023347b33211a56a7
syoh89 <syoh89@jasonsystem.co.kr>
2025-05-20 12:36:07 +0900

    feature/FRONT-367 : Button - Button.tsx 에서 data-slot 제거 - normalize.css 에서 layer @base에 있는 부분 제거

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -16,7 +16,7 @@ function Button({
     asChild?: boolean;
   }) {
   const Comp = asChild ? Slot : 'button';
-  const buttonProps = asChild ? { ...props } : { type: props?.type || 'button', 'data-slot': 'button', ...props };
+  const buttonProps = asChild ? { ...props } : { type: props?.type || 'button', ...props };
 
   return <Comp className={cn(buttonVariants({ variant, size, disabled, className }))} {...buttonProps} />;
 }
