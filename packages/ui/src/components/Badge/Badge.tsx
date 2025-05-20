a079c88347bde0294f0c9fff646c24dd1d677080
syoh89 <syoh89@jasonsystem.co.kr>
2025-05-20 16:33:57 +0900

    feature/FRONT-379 : [jui-frontend][UI 1차] Shadcn base 컴포넌트 만들기 - Badge - badgeVariants.ts 분리

--- packages/ui/src/components/Badge/Badge.tsx
+++ packages/ui/src/components/Badge/Badge.tsx
@@ -1,31 +1,19 @@
 import * as React from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
+import { type VariantProps } from 'class-variance-authority';
 
 import { cn } from '../../lib/utils';
+import { Slot } from '@radix-ui/react-slot';
+import badgeVariants from '@common/ui/components/Badge/badgeVariants';
 
-const badgeVariants = cva(
-  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
-  {
-    variants: {
-      variant: {
-        default: `border-transparent bg-cyan-400 text-gray-0 shadow hover:bg-primary/80`,
-        secondary: 'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        destructive: 'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
-        outline: 'text-foreground',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
-    },
-  },
-);
+function Badge({
+  className,
+  variant,
+  asChild = false,
+  ...props
+}: React.ComponentProps<'span'> & VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : 'span';
 
-export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {}
-
-function Badge({ className, variant, ...props }: BadgeProps) {
-  return <div className={cn(badgeVariants({ variant }), className)} {...props} />;
+  return <Comp data-slot="badge" className={cn(badgeVariants({ variant }), className)} {...props} />;
 }
-
-export { badgeVariants };
 
 export default Badge;
--- packages/ui/src/components/Badge/index.ts
+++ packages/ui/src/components/Badge/index.ts
@@ -1,1 +1,2 @@
-export { default as Badge, badgeVariants } from './Badge';
+export { default as Badge } from './Badge';
+export { default as badgeVariants } from './badgeVariants';
