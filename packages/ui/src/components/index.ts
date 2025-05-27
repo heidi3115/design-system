d8f5faaae1462304c9f7b131d8896c8b63efdabb
mskim94 <mskim@jasonsystem.co.kr>
2025-05-27 13:40:01 +0900

    feature/FRONT-375 [jui-frontend][UI 1차] Shadcn base 컴포넌트 만들기 - Label

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -3,3 +3,4 @@ export { Badge, badgeVariants } from './Badge';
 export { Input, inputVariants } from './Input';
 export { Checkbox } from './Checkbox';
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './Card';
+export { Label, labelVariants } from './Label';
