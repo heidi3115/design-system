db560ffdcc40fe6f6efe5b19cdbd10991e634c8f
syoh89 <syoh89@jasonsystem.co.kr>
2025-05-29 16:01:33 +0900

    feature/FRONT-379 : [jui-frontend][UI 1차] Shadcn base 컴포넌트 만들기 - Badge - Badge의 제한된 컴포넌트 내역 전체적인 수정(분리한 Badge는 asChild 불가 등) - Badge/index.ts 정리

--- packages/ui/src/components/Badge/index.ts
+++ packages/ui/src/components/Badge/index.ts
@@ -1,19 +1,13 @@
 export { default as badgeVariants } from './badgeVariants';
 
-export { default as Badge } from './Badge';
-export type { BadgePropsType } from './Badge';
+export { default as Badge, type BadgePropsType } from './Badge';
 
-export { default as StateBadge } from './StateBadge';
-export type { StateBadgePropsType } from './StateBadge';
+export { default as StateBadge, type StateBadgePropsType } from './StateBadge';
 
-export { default as ScoringBadge } from './ScoringBadge';
-export type { ScoringBadgePropsType } from './ScoringBadge';
+export { default as ScoringBadge, type ScoringBadgePropsType } from './ScoringBadge';
 
-export { default as GradeBadge } from './GradeBadge';
-export type { GradeBadgePropsType } from './GradeBadge';
+export { default as GradeBadge, type GradeBadgePropsType } from './GradeBadge';
 
-export { default as CountBadge } from './CountBadge';
-export type { CountBadgePropsType } from './CountBadge';
+export { default as CountBadge, type CountBadgePropsType } from './CountBadge';
 
-export { default as TextBadge } from './TextBadge';
-export type { TextBadgePropsType } from './TextBadge';
+export { default as TextBadge, type TextBadgePropsType } from './TextBadge';
