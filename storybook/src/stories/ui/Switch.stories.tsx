079189b34770e6675934f01b02fb1d8762403e19
mskim94 <mskim@jasonsystem.co.kr>
2025-06-02 16:52:03 +0900

    feature/FRONT-376[jui-frontend][UI 1차] Shadcn base 컴포넌트 만들기 - Switch 기본 색상 지정 (primary, secondary, error), storybook 수정

--- storybook/src/stories/ui/Switch.stories.tsx
+++ storybook/src/stories/ui/Switch.stories.tsx
@@ -2,19 +2,33 @@ import { Button, Switch } from '@common/ui/components';
 import type { Meta, StoryObj } from '@storybook/react';
 import { type ComponentProps, useRef, useState } from 'react';
 import { action } from '@storybook/addon-actions';
+import { switchVariants } from '@common/ui/components/Switch/Switch.tsx';
 
 const meta: Meta<typeof Switch> = {
   title: 'UI/Switch',
   component: Switch,
+  parameters: {
+    controls: {
+      exclude: ['defaultChecked'],
+    },
+  },
   argTypes: {
-    // children: { control: 'text', description: '텍스트' },
     checkedRef: {
       table: { disable: true },
+    },
+    disabled: {
+      description: '비활성 여부',
+    },
+    variant: {
+      control: 'radio',
+      options: Object.keys(switchVariants.variants.variant),
+      description: '스위치 색상 변경',
     },
   },
   args: {
     disabled: false,
-    // children: 'Switch on/off',
+    variant: 'primary',
+    defaultChecked: true,
   },
 };
 
@@ -43,32 +57,59 @@ export const Default: Story = {
   render: Template,
 };
 
-export const Controlled: Story = {
-  args: {},
-  argTypes: {
-    // children: {
-    //   table: { disable: true },
-    // },
-  },
+export const Variant: Story = {
   parameters: {
     docs: {
       description: {
-        story: '',
+        story: 'variant 값에 따른 스타일 변경 예시',
+      },
+    },
+    controls: {
+      exclude: ['variant'],
+    },
+  },
+  render: (args) => {
+    return (
+      <div className="flex flex-col gap-5 w-60 justify-between font-bold text-sm">
+        <div className="flex flex-col gap-2">
+          <span>Primary</span>
+          <Switch {...args} variant="primary" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <span>Secondary</span>
+          <Switch {...args} variant="secondary" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <span>Error</span>
+          <Switch {...args} variant="error" />
+        </div>
+      </div>
+    );
+  },
+};
+
+export const Controlled: Story = {
+  argTypes: {},
+  args: {},
+  parameters: {
+    docs: {
+      description: {
+        story: '제어형(Controlled) 사용 예시',
       },
       disable: true,
     },
   },
-  render: ({ ...args }) => {
-    const [isPress, setIsPress] = useState(false);
+  render: ({ onCheckedChange, ...args }) => {
+    const [isChecked, setIsChecked] = useState(false);
     const checkedRef = useRef(null);
 
     const logControlledChange = action('제어형 onChange 발생');
     const logUncontrolledConfirm = action('비제어형 확인');
 
-    const controlledhandleChange = (press: boolean) => {
-      setIsPress(press);
-      logControlledChange(press);
-      // checkedRef?.(press);
+    const controlledhandleChange = (check: boolean) => {
+      setIsChecked(check);
+      logControlledChange(check);
+      onCheckedChange?.(check);
     };
 
     const unControlledhandleChange = () => {
@@ -78,20 +119,20 @@ export const Controlled: Story = {
     return (
       <div className="flex flex-col gap-4">
         <div className="flex flex-col gap-2">
-          <span className="text-sm font-bold">제어형 Switch</span>
+          <span className="text-sm font-bold">제어형</span>
           <div className="w-3xs">
-            <Switch {...args} checked={isPress} onCheckedChange={controlledhandleChange}>
+            <Switch {...args} checked={isChecked} onCheckedChange={controlledhandleChange}>
               제어
             </Switch>
           </div>
         </div>
         <div className="flex flex-col gap-2">
-          <span className="text-sm font-bold">비제어형 Switch(변경 후 외부 클릭)</span>
+          <span className="text-sm font-bold">비제어형</span>
           <div className="w-3xs flex gap-2">
-            {/*<Switch {...args} checkedRef={checkedRef}>*/}
-            {/*  비제어*/}
-            {/*</Switch>*/}
-            <Button onClick={unControlledhandleChange}>비제어 확인 Click</Button>
+            <Switch {...args} checkedRef={checkedRef}>
+              비제어
+            </Switch>
+            <Button onClick={unControlledhandleChange}>비제어 확인</Button>
           </div>
         </div>
       </div>
