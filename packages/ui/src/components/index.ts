06ced4ab03f33759d0b36c70f6b16df1b8c66bf3
syoh89 <syoh89@jasonsystem.co.kr>
2025-06-09 13:22:20 +0900

    feature/FRONT-381 : [jui-frontend][UI 1차] Shadcn base 컴포넌트 만들기 - Tooltip  - TooltipParts 추가

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -44,3 +44,4 @@ export {
 } from './AlertDialog';
 export { Label } from './Label';
 export { Switch } from './Switch';
+export { TooltipRoot, TooltipTrigger, TooltipContent, TooltipProvider } from './Tooltip';
