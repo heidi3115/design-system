99f3d5c6309014b329163fcea98b50d0af6bdd6b
msjang <msjang24@jasonsystem.co.kr>
2025-06-13 17:50:48 +0900

    fix: 린트테스트

--- apps/next-app/app/ms/page.tsx
+++ apps/next-app/app/ms/page.tsx
@@ -137,7 +137,6 @@ export default function Page() {
             <Button
               onClick={() => {
                 setIsOpenPopover(!isOpenPopover);
-                console.log('aa');
               }}>
               다른곳 클릭
             </Button>
