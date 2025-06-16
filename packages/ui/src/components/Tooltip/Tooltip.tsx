8cd32197d122519676b551a12bbe567bb1ecd275
syoh89 <syoh89@jasonsystem.co.kr>
2025-06-16 16:21:29 +0900

    feature/FRONT-381 : [jui-frontend][UI 1차] Shadcn base 컴포넌트 만들기 - Tooltip - Tooltip.tsx 에 'use client' 추가

--- packages/ui/src/components/Tooltip/Tooltip.tsx
+++ packages/ui/src/components/Tooltip/Tooltip.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { isValidElement, useEffect, useImperativeHandle, useState } from 'react';
 import { type VariantProps } from 'tailwind-variants';
 import { cn } from '@common/ui/lib/utils';
