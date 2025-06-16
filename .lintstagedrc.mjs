13e3ba066942099eb3cc33a748dee29c024765ea
msjang <msjang24@jasonsystem.co.kr>
2025-06-16 09:09:29 +0900

    fix: lint-staged 변경

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,11 @@
 const lintstagedrc = {
   '*.{ts,tsx,js,jsx,json}': ['eslint --fix', 'prettier --write'],
   '*.{md,css,scss}': ['prettier --write'],
+  'packages/ui/**/*.{ts,tsx,js,jsx,json}': ['pnpm ui lint'],
+  'packages/utils/**/*.{ts,tsx,js,jsx,json}': ['pnpm utils lint'],
+  'apps/next-app/**/*.{ts,tsx,js,jsx,json}': ['pnpm next-app eslint'],
+  'apps/reaect-app/**/*.{ts,tsx,js,jsx,json}': ['pnpm react-app eslint'],
+  'storybook**/*.{ts,tsx,js,jsx,json}': ['pnpm storybook lint'],
 };
 
 export default lintstagedrc;
