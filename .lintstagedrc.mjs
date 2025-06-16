2a0a295987a5ba41b70e079f191a0475a37216d3
msjang <msjang24@jasonsystem.co.kr>
2025-06-16 09:02:27 +0900

    fix: 린트 테스트

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,7 @@
 const lintstagedrc = {
   '*.{ts,tsx,js,jsx,json}': ['eslint --fix --quiet', 'prettier --write'],
   '*.{md,css,scss}': ['prettier --write'],
+  'packages/ui/**/*.{ts,tsx,js,jsx,json}': ['pnpm ui lint'],
 };
 
 export default lintstagedrc;
