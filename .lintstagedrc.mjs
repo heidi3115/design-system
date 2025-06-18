aae46a1572fcc2c9bfb6b81cd1322fc78b814088
msjang <msjang24@jasonsystem.co.kr>
2025-06-18 14:03:41 +0900

    fix: variants 분리

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -6,24 +6,7 @@ const lintstagedrc = {
   'apps/react-app/**/*.{ts,tsx,js,jsx}': ['pnpm react-app lint'],
   'storybook/**/*.{ts,tsx,js,jsx}': ['pnpm storybook lint'],
 
-  // '**/*.{ts,tsx,js,jsx,json}': {
-  //   title: 'Run eslint fix and prettier',
-  //   task: async () => {
-  //     // eslint --fix 실행
-  //     await execAsync('eslint', ['--fix', '--ext', '.ts,.tsx,.js,.jsx,.json'], { stdio: 'inherit' });
-  //     // prettier 실행
-  //     await execAsync('prettier', ['--write', '.'], { stdio: 'inherit' });
-  //   },
-  //   globOptions: {
-  //     ignore: [
-  //       'packages/ui/**/*',
-  //       'packages/utils/**/*',
-  //       'apps/next-app/**/*',
-  //       'apps/react-app/**/*',
-  //       'storybook/**/*',
-  //     ],
-  //   },
-  // },
+  '**/*.{ts,tsx,js,jsx,json}': ['eslint --fix', 'prettier --write'],
 };
 
 export default lintstagedrc;
