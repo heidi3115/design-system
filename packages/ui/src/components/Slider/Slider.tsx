774ba4bf0d821a8d24e5b2836a558911fd24db92
syoh89 <syoh89@jasonsystem.co.kr>
2025-06-25 10:34:55 +0900

    feature/FRONT-395 : [jui-frontend][UI 3차] Shadcn base 컴포넌트 만들기 - Slider - packages/ui 에 pnpm add @radix-ui/react-slider 추가 - SliderParts.tsx, Slider.tsx, sliderVariants 추가 및 index.ts 등록 - slider 뼈대 구성.

--- packages/ui/src/components/Slider/Slider.tsx
+++ packages/ui/src/components/Slider/Slider.tsx
@@ -1,51 +1,59 @@
 'use client';
 
 import * as React from 'react';
-import * as SliderPrimitive from '@radix-ui/react-slider';
 
 import { cn } from '../../lib/utils';
-import type { SliderRootProps } from '@common/ui';
+import { SliderRange, SliderRoot, type SliderRootProps, SliderThumb, SliderTrack } from './SliderParts';
+import { sliderVariants } from '@common/ui/components';
+import type { VariantProps } from 'tailwind-variants';
 
-export type SliderProps = SliderRootProps & {};
+export type SliderProps = SliderRootProps & VariantProps<typeof sliderVariants> & {};
 
-function Slider({ className, defaultValue, value, min = 0, max = 100, ...props }: SliderProps) {
+export default function Slider({
+  variant = 'default',
+  size = 'default',
+  orientation = 'horizontal',
+  disabled = false,
+  defaultValue,
+  value,
+  min = 0,
+  max = 100,
+  className,
+  ...props
+}: SliderProps) {
+  const { base, root, track, range, thumb } = sliderVariants({ variant, size, orientation, disabled });
+  const baseClass = base();
+  const rootClass = root();
+  const trackClass = track();
+  const rangeClass = range();
+  const thumbClass = thumb();
+
+  console.warn('\n\nSliderDefault\nbaseClass :', baseClass);
+  console.warn('rootClass :', rootClass);
+  console.warn('trackClass :', trackClass);
+  console.warn('rangeClass :', rangeClass);
+  console.warn('thumbClass :', thumbClass);
+
   const _values = React.useMemo(
     () => (Array.isArray(value) ? value : Array.isArray(defaultValue) ? defaultValue : [min, max]),
     [value, defaultValue, min, max],
   );
 
   return (
-    <SliderPrimitive.Root
+    <SliderRoot
       data-slot="slider"
       defaultValue={defaultValue}
       value={value}
       min={min}
       max={max}
-      className={cn(
-        'relative flex w-full touch-none items-center select-none data-[disabled]:opacity-50',
-        'data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col',
-        className,
-      )}
+      className={cn(baseClass, rootClass, className)}
       {...props}>
-      <SliderPrimitive.Track
-        data-slot="slider-track"
-        className={cn(
-          'bg-muted relative grow overflow-hidden rounded-full data-[orientation=horizontal]:h-1.5 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5',
-        )}>
-        <SliderPrimitive.Range
-          data-slot="slider-range"
-          className={cn('bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full')}
-        />
-      </SliderPrimitive.Track>
+      <SliderTrack data-slot="slider-track" className={cn(baseClass, trackClass)}>
+        <SliderRange data-slot="slider-range" className={cn(baseClass, rangeClass)} />
+      </SliderTrack>
       {Array.from({ length: _values.length }, (_, index) => (
-        <SliderPrimitive.Thumb
-          data-slot="slider-thumb"
-          key={index}
-          className="border-primary bg-background ring-ring/50 block size-4 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
-        />
+        <SliderThumb data-slot="slider-thumb" key={index} className={cn(baseClass, thumbClass)} />
       ))}
-    </SliderPrimitive.Root>
+    </SliderRoot>
   );
 }
-
-export default { Slider };
