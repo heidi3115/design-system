e9590772b41798fb9ae1116ccdf81ff472a35ecc
syoh89 <syoh89@jasonsystem.co.kr>
2025-07-03 12:47:32 +0900

    feature/FRONT-395 : [jui-frontend][UI 3차] Shadcn base 컴포넌트 만들기 - Slider - label 정렬 추가

--- packages/ui/src/components/Slider/Slider.tsx
+++ packages/ui/src/components/Slider/Slider.tsx
@@ -272,8 +272,8 @@ function Slider({
                       <span
                         className={cn(
                           'absolute',
-                          isHorizontal ? 'top-full -translate-x-1/2' : '-translate-y-1/2',
-                          'text-sm',
+                          isHorizontal ? 'top-full left-1/2 -translate-x-1/2' : 'left-full top-1/2 -translate-y-1/2',
+                          'w-max text-xs',
                           labelClass,
                         )}
                         style={
