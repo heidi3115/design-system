d7fd72c05d92d312330b5ca45cf1fa340ac31286
mskim94 <mskim@jasonsystem.co.kr>
2025-07-07 14:26:17 +0900

    feature/FRONT-408 [jui-frontend][UI 3차] 사이드바 스타일링1 - 404 에러 페이지 추가

---
+++ apps/next-app/app/[...notfound]/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h2 className="text-xl">페이지 찾을수 없음</h2>
+    </div>
+  );
+};
+
+export default NotFound;
