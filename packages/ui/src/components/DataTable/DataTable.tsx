9ac300f3f28c077f2feca0718f1f55584ca53711
mskim94 <mskim@jasonsystem.co.kr>
2025-07-15 15:13:41 +0900

    feature/FRONT-416 Data table - Data Table 컴포넌트에 제네릭 타입 추가 및 data,columns props로 분리

--- packages/ui/src/components/DataTable/DataTable.tsx
+++ packages/ui/src/components/DataTable/DataTable.tsx
@@ -17,78 +17,16 @@ import {
 import { TableHeader, Table, TableRow, TableHead, TableCell, TableBody } from '@common/ui';
 import { useState } from 'react';
 
-export type Payment = {
-  id: string;
-  amount: number;
-  status: 'pending' | 'processing' | 'success' | 'failed';
-  email: string;
+type DataTableProps<T> = {
+  data: T[];
+  columns: ColumnDef<T>[];
 };
 
-export const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: 'status',
-    header: 'Status',
-    cell: ({ row }) => <div className="capitalize">{row.getValue('status')}</div>,
-  },
-  {
-    accessorKey: 'email',
-    header: 'Email',
-    cell: ({ row }) => <div className="lowercase">{row.getValue('email')}</div>,
-  },
-  {
-    accessorKey: 'amount',
-    header: 'Amount',
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue('amount'));
-
-      const formatted = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      }).format(amount);
-
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
-  },
-];
-
-export function DataTableDemo() {
+export function DataTable<T>({ data, columns }: DataTableProps<T>) {
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
   const [rowSelection, setRowSelection] = useState({});
-
-  const data: Payment[] = [
-    {
-      id: 'm5gr84i9',
-      amount: 316,
-      status: 'success',
-      email: 'ken99@example.com',
-    },
-    {
-      id: '3u1reuv4',
-      amount: 242,
-      status: 'success',
-      email: 'Abe45@example.com',
-    },
-    {
-      id: 'derv1ws0',
-      amount: 837,
-      status: 'processing',
-      email: 'Monserrat44@example.com',
-    },
-    {
-      id: '5kma53ae',
-      amount: 874,
-      status: 'success',
-      email: 'Silas22@example.com',
-    },
-    {
-      id: 'bhqecj4p',
-      amount: 721,
-      status: 'failed',
-      email: 'carmella@example.com',
-    },
-  ];
 
   const table = useReactTable({
     data,
