82591f5966705dc998709544a830afee790ea824
mskim94 <mskim@jasonsystem.co.kr>
2025-07-18 16:27:37 +0900

    feature/FRONT-418 Tanstack Table - 예시 데이터 정리

--- apps/next-app/app/mskim/table/page.tsx
+++ apps/next-app/app/mskim/table/page.tsx
@@ -6,115 +6,40 @@ import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, Table
 import { useEffect, useState } from 'react';
 
 export type Scenario = {
-  scnrIdx: string;
-  scnrDttIdx: string;
-  scnrCd: string;
   scnrNm: string;
-  scnrCls: string;
-  dttTrgTyp: string;
-  dngrGrd: string;
-  dttTyp: string;
-  respMode: string;
-  oprStt: string;
-  msrCycl: string;
-  explnUseYn: string;
-  modUser: string;
-  modUserNm: string;
-  modDt: string;
   regUser: string;
   regUserNm: string;
   regDt: string;
-  alrmYn: string;
 };
 
 export default function Page() {
   const [serverData, setServerData] = useState([
     {
-      scnrIdx: 'gLxtmTtq1nzgQXiM8TT5jw==',
-      scnrDttIdx: 'AYb4MDUINNoV4IrJh4UxIg==',
-      scnrCd: '212',
       scnrNm: '[QA-3560] 테스트 시나리오',
-      scnrCls: 'QA',
-      dttTrgTyp: '011001',
-      dngrGrd: '012001',
-      dttTyp: '016001',
-      respMode: '013001',
-      oprStt: '014001',
-      msrCycl: '41 16 * * * ',
-      explnUseYn: 'N',
-      modUser: 'admin',
-      modUserNm: '관리자',
-      modDt: '2025-07-14 16:57:23',
       regUser: 'hycho',
       regUserNm: '조홍연',
       regDt: '2025-03-26 15:48:46',
-      alrmYn: 'N',
     },
     {
-      scnrIdx: 'i5DeKy0RP3wcBpK/CPAudg==',
-      scnrDttIdx: 'HGvQI8mqXT36LndysbGm8Q==',
-      scnrCd: '177',
       scnrNm: '[1112] AI 다중 임계치 테스트 - 커스텀커맨드',
-      scnrCls: 'QA|E-Mail|Works',
-      dttTrgTyp: '011001',
-      dngrGrd: '012003',
-      dttTyp: '016009',
-      respMode: '013001',
-      oprStt: '014001',
-      msrCycl: '27 12 * * *',
-      explnUseYn: 'N',
-      modUser: 'admin',
-      modUserNm: '관리자',
-      modDt: '2025-07-08 15:41:19',
       regUser: 'admin',
       regUserNm: '관리자',
       regDt: '2024-11-28 10:38:05',
-      alrmYn: 'N',
     },
   ]);
 
   const clientData = [
     {
-      scnrIdx: 'gLxtmTtq1nzgQXiM8TT5jw==',
-      scnrDttIdx: 'AYb4MDUINNoV4IrJh4UxIg==',
-      scnrCd: '212',
       scnrNm: '[QA-3560] 테스트 시나리오',
-      scnrCls: 'QA',
-      dttTrgTyp: '011001',
-      dngrGrd: '012001',
-      dttTyp: '016001',
-      respMode: '013001',
-      oprStt: '014001',
-      msrCycl: '41 16 * * * ',
-      explnUseYn: 'N',
-      modUser: 'admin',
-      modUserNm: '관리자',
-      modDt: '2025-07-14 16:57:23',
       regUser: 'hycho',
       regUserNm: '조홍연',
       regDt: '2025-03-26 15:48:46',
-      alrmYn: 'N',
     },
     {
-      scnrIdx: 'i5DeKy0RP3wcBpK/CPAudg==',
-      scnrDttIdx: 'HGvQI8mqXT36LndysbGm8Q==',
-      scnrCd: '177',
       scnrNm: '[1112] AI 다중 임계치 테스트 - 커스텀커맨드',
-      scnrCls: 'QA|E-Mail|Works',
-      dttTrgTyp: '011001',
-      dngrGrd: '012003',
-      dttTyp: '016009',
-      respMode: '013001',
-      oprStt: '014001',
-      msrCycl: '27 12 * * *',
-      explnUseYn: 'N',
-      modUser: 'admin',
-      modUserNm: '관리자',
-      modDt: '2025-07-08 15:41:19',
       regUser: 'admin',
       regUserNm: '관리자',
       regDt: '2024-11-28 10:38:05',
-      alrmYn: 'N',
     },
   ];
 
@@ -146,46 +71,16 @@ export default function Page() {
 
     const dummyData: Scenario[] = [
       {
-        scnrIdx: 'gLxtmTtq1nzgQXiM8TT5jw==',
-        scnrDttIdx: 'AYb4MDUINNoV4IrJh4UxIg==',
-        scnrCd: '212',
         scnrNm: '[QA-3560] 새로운 제목2',
-        scnrCls: 'QA',
-        dttTrgTyp: '011001',
-        dngrGrd: '012001',
-        dttTyp: '016001',
-        respMode: '013001',
-        oprStt: '014001',
-        msrCycl: '41 16 * * * ',
-        explnUseYn: 'N',
-        modUser: 'admin',
-        modUserNm: '관리자',
-        modDt: '2025-07-14 16:57:23',
         regUser: 'hycho',
         regUserNm: '새로운데이터',
         regDt: '2025-03-26 15:48:46',
-        alrmYn: 'N',
       },
       {
-        scnrIdx: 'i5DeKy0RP3wcBpK/CPAudg==',
-        scnrDttIdx: 'HGvQI8mqXT36LndysbGm8Q==',
-        scnrCd: '177',
         scnrNm: '[1112]새로운 제목2',
-        scnrCls: 'QA|E-Mail|Works',
-        dttTrgTyp: '011001',
-        dngrGrd: '012003',
-        dttTyp: '016009',
-        respMode: '013001',
-        oprStt: '014001',
-        msrCycl: '27 12 * * *',
-        explnUseYn: 'N',
-        modUser: 'admin',
-        modUserNm: '관리자',
-        modDt: '2025-07-08 15:41:19',
         regUser: 'admin',
         regUserNm: '새로운데이터2',
         regDt: '2024-11-28 10:38:05',
-        alrmYn: 'N',
       },
     ];
 
