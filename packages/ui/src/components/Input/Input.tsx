348f5d21cbbb29414a47fc7ca6396fb0af47a743
msjang <msjang24@jasonsystem.co.kr>
2025-07-22 17:10:41 +0900

    fix: default 와 secondary 옵션 추가

--- packages/ui/src/components/Input/Input.tsx
+++ packages/ui/src/components/Input/Input.tsx
@@ -53,7 +53,7 @@ function Input({
 
   return (
     <div>
-      <div className={cn('relative group min-h-7 bg-juiBackground-input', className)}>
+      <div className={cn('relative group min-h-7', underline !== 'none' && 'bg-juiBackground-input', className)}>
         {IconLeft && (
           <span
             className={cn(
