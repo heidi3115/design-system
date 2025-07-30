c06cd97e209c9e8b1888b7ecf6e2c8c80f409b26
msjang <msjang24@jasonsystem.co.kr>
2025-07-30 09:51:34 +0900

    fix: formatter ko 로 고정

--- packages/ui/src/components/Calendar/Calendar.tsx
+++ packages/ui/src/components/Calendar/Calendar.tsx
@@ -50,7 +50,7 @@ function Calendar({
       )}
       captionLayout={captionLayout}
       formatters={{
-        formatMonthDropdown: (date) => date.toLocaleString('default', { month: 'short' }),
+        formatMonthDropdown: (date) => date.toLocaleString('ko', { month: 'short' }),
         ...formatters,
       }}
       classNames={{
