f8202b6f126e6b10f20ba35a445fb7a00a895b2d
msjang <msjang24@jasonsystem.co.kr>
2025-08-28 12:59:03 +0900

    fix: jest test

--- packages/ui/src/components/Label/Label.test.tsx
+++ packages/ui/src/components/Label/Label.test.tsx
@@ -2,10 +2,9 @@ import { render, screen } from '@testing-library/react';
 import Label from './Label';
 
 describe('Label', () => {
-  test('Label 컴포넌트를 렌더링 한다', () => {
+  test('Label 컴포넌트를 렌더링 합니다.', () => {
     render(<Label htmlFor="email-input">label</Label>);
 
-    //test
     const label = screen.getByText('label');
 
     expect(label).toBeInTheDocument();
