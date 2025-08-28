89266e8210a0756e3d8ebc4355285d7fff420f09
msjang <msjang24@jasonsystem.co.kr>
2025-08-28 13:14:50 +0900

    fix: ts 테스트

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,6 @@
 const lintstagedrc = {
   '*.{md,css,scss}': ['prettier --write'],
-  'packages/ui/**/*.{ts,tsx,js,jsx}': ['pnpm ui lint', 'pnpm ui test'],
+  'packages/ui/**/*.{ts,tsx,js,jsx}': ['pnpm ui lint', 'pnpm ui test:lintStage'],
   'packages/utils/**/*.{ts,tsx,js,jsx}': ['pnpm utils lint'],
   'apps/next-app/**/*.{ts,tsx,js,jsx}': ['pnpm next-app eslint'],
   'apps/react-app/**/*.{ts,tsx,js,jsx}': ['pnpm react-app lint'],
